ffc88b5c7cfefb160914228ddd62bf7c4fbd47ac
stephanie benitez <stephaniebee94@gmail.com>
2020-07-07 01:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+function happyHolidays (
+  {
+    return( `Happy Birthday + ${name})
+  }
+happyHolidays(stephanie)
+happyHolidaysTo(name)
+happyCustomHolidayTo(holiday, name)
+holidayCountdown(holiday, days)
